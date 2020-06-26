beae85b1c0a1b91fa5a3e55f8310da079dab2b2e
Leonardo Ruiz <leonardo.ruizsua@gmail.com>
2020-06-26 12:11:36 -0500

    Updates

---
+++ precache-manifest.dff5494bb13edcb39aceaabe683a0154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee9c6c2b3a875dba3b9d981093f78a3",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "8c1b5a95e6903e8b4620",
+    "url": "/react-todo-list/static/css/main.14c1d2cb.chunk.css"
+  },
+  {
+    "revision": "ee205fbab5174c091753",
+    "url": "/react-todo-list/static/js/2.1a32253b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.1a32253b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1b5a95e6903e8b4620",
+    "url": "/react-todo-list/static/js/main.c637c165.chunk.js"
+  },
+  {
+    "revision": "4a2e409b53d6c40e0e96",
+    "url": "/react-todo-list/static/js/runtime-main.90e5866e.js"
+  }
+]);
